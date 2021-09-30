2f6dfe3e686fc2563577ae54c759483bf72701fa
Miguel <krhono62@gmail.com>
2021-09-29 21:07:57 -0500

    It works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Firebase from './BD/Firebase';
+import firebase from 'firebase/compat/app';
+import "firebase/compat/auth";
 function App() {
+  firebase.auth().onAuthStateChanged(user => {
+    console.log("Holi soy el usuario : " + user);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Holi</h1>
     </div>
   );
 }
---
+++ src/BD/Firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/compat/app';
+const firebaseConfig = {
+    apiKey: "AIzaSyDO-uFUWDnhoC8tcarXZy6mbtO7lcQSm-I",
+    authDomain: "crud-d41bb.firebaseapp.com",
+    projectId: "crud-d41bb",
+    storageBucket: "crud-d41bb.appspot.com",
+    messagingSenderId: "372576584670",
+    appId: "1:372576584670:web:ba2c1b6854a2e12e68134f"
+  };
+  
+  // Initialize Firebase
+  export default firebase.initializeApp(firebaseConfig);
