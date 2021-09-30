2df65a5b57a0f3e72f9d08e209df333a17f55596
Miguel <krhono62@gmail.com>
2021-09-30 09:45:37 -0500

    Añadi el CREATE

--- src/BD/Firebase.js
+++ src/BD/Firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/compat/app';
 const firebaseConfig = {
-    apiKey: "AIzaSyDO-uFUWDnhoC8tcarXZy6mbtO7lcQSm-I",
-    authDomain: "crud-d41bb.firebaseapp.com",
-    projectId: "crud-d41bb",
-    storageBucket: "crud-d41bb.appspot.com",
-    messagingSenderId: "372576584670",
-    appId: "1:372576584670:web:ba2c1b6854a2e12e68134f"
-  };
+  apiKey: "AIzaSyBwVp5Q1qN4xVMVOx6sHqseP4T_mFkFoso",
+  authDomain: "crud-fbf18.firebaseapp.com",
+  projectId: "crud-fbf18",
+  storageBucket: "crud-fbf18.appspot.com",
+  messagingSenderId: "560541652720",
+  appId: "1:560541652720:web:8802a40e829fd03c869435"
+};
   
   // Initialize Firebase
   export default firebase.initializeApp(firebaseConfig);
